5ca37b340f84f473b501bf794ba4e02e55ad1544
nahaktarun <tarunnahak25@gmail.com>
2025-06-07 15:30:50 +0530

    refactored move formatting logic to Details ui component

--- internal/services/app.go
+++ internal/services/app.go
@@ -12,8 +12,6 @@ import (
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
-	"golang.org/x/text/language"
-	"golang.org/x/text/message"
 )
 
 var (
@@ -46,6 +44,7 @@ type AppService struct {
 func NewAppService() AppServiceInterface {
 	app := tview.NewApplication()
 	themeService := theme.NewTheme()
+	brewService := NewBrewService()
 
 	appService := &AppService{
 		app:    app,
@@ -58,7 +57,7 @@ func NewAppService() AppServiceInterface {
 		showOnlyOutdated:  false,
 		brewVersion:       "-",
 
-		BrewService:       NewBrewService(),
+		BrewService:       brewService,
 		CommandService:    NewCommandService(),
 		SelfUpdateService: NewSelfUpdateService(),
 	}
@@ -156,137 +155,11 @@ func (s *AppService) search(searchText string, scrollToTop bool) {
 
 func (s *AppService) setDetails(info *models.Formula) {
 	if info == nil {
-		s.layout.GetDetails().SetContent("")
+		s.layout.GetDetails().SetContent(nil)
 		return
 	}
 
-	// Installation status with colors
-	installedStatus := "[red]Not installed[-]"
-	installedIcon := "✗"
-	if len(info.Installed) > 0 {
-		installedStatus = "[green]Installed[-]"
-		installedIcon = "✓"
-
-		if info.Outdated {
-			installedStatus = "[orange]Update available[-]"
-			installedIcon = "⟳"
-		}
-	}
-
-	// Basic information with icons
-	basicInfo := fmt.Sprintf(
-		"[yellow::b]%s %s[-]\n\n"+
-			"[blue]• Name:[-] %s\n"+
-			"[blue]• Version:[-] %s\n"+
-			"[blue]• Status:[-] %s %s\n"+
-			"[blue]• Tap:[-] %s\n"+
-			"[blue]• License:[-] %s\n\n"+
-			"[yellow::b]Description[-]\n%s\n\n"+
-			"[blue]• Homepage:[-] %s",
-		info.Name, installedIcon,
-		info.FullName,
-		info.Versions.Stable,
-		installedStatus, s.getPackageVersionInfo(info),
-		info.Tap,
-		info.License,
-		info.Description,
-		info.Homepage,
-	)
-
-	// Installation details
-	installDetails := s.getPackageInstallationDetails(info)
-
-	// Dependencies with improved formatting
-	dependenciesInfo := s.getDependenciesInfo(info)
-
-	analyticsInfo := s.getAnalyticsInfo(info)
-
-	s.layout.GetDetails().SetContent(strings.Join([]string{basicInfo, installDetails, dependenciesInfo, analyticsInfo}, "\n\n"))
-}
-
-func (s *AppService) getPackageVersionInfo(info *models.Formula) string {
-	if len(info.Installed) == 0 {
-		return ""
-	}
-
-	installedVersion := info.Installed[0].Version
-	stableVersion := info.Versions.Stable
-
-	// Revision version
-	if strings.HasPrefix(installedVersion, stableVersion+"_") {
-		return fmt.Sprintf("([green]%s[-])", installedVersion)
-	} else if installedVersion == stableVersion {
-		return fmt.Sprintf("([green]%s[-])", installedVersion)
-	} else if installedVersion < stableVersion || info.Outdated {
-		return fmt.Sprintf("([orange]%s[-] → [green]%s[-])",
-			installedVersion, stableVersion)
-	}
-
-	// Other cases
-	return fmt.Sprintf("([green]%s[-])", installedVersion)
-}
-
-func (s *AppService) getPackageInstallationDetails(info *models.Formula) string {
-	if len(info.Installed) == 0 {
-		return "[yellow::b]Installation[-]\nNot installed"
-	}
-
-	packagePrefix := s.BrewService.GetPrefixPath() + "/" + info.Name
-	installedOnRequest := "No"
-	if info.Installed[0].InstalledOnRequest {
-		installedOnRequest = "Yes"
-	}
-
-	installedAsDependency := "No"
-	if info.Installed[0].InstalledAsDependency {
-		installedAsDependency = "Yes"
-	}
-
-	return fmt.Sprintf(
-		"[yellow::b]Installation Details[-]\n"+
-			"[blue]• Path:[-] %s\n"+
-			"[blue]• Installed on request:[-] %s\n"+
-			"[blue]• Installed as dependency:[-] %s\n"+
-			"[blue]• Installed version:[-] %s",
-		packagePrefix,
-		installedOnRequest,
-		installedAsDependency,
-		info.Installed[0].Version,
-	)
-}
-
-func (s *AppService) getDependenciesInfo(info *models.Formula) string {
-	title := "[yellow::b]Dependencies[-]\n"
-
-	if len(info.Dependencies) == 0 {
-		return title + "No dependencies"
-	}
-
-	// Format dependencies in multiple columns or with separators
-	deps := ""
-	for i, dep := range info.Dependencies {
-		deps += dep
-		if i < len(info.Dependencies)-1 {
-			if (i+1)%3 == 0 {
-				deps += "\n"
-			} else {
-				deps += ", "
-			}
-		}
-	}
-
-	return title + deps
-}
-
-func (s *AppService) getAnalyticsInfo(info *models.Formula) string {
-	title := "[yellow::b]Analytics[-]\n"
-
-	p := message.NewPrinter(language.English)
-
-	title += fmt.Sprintf("[blue]• 90d Global Rank:[-] %s\n", p.Sprintf("%d", info.Analytics90dRank))
-	title += fmt.Sprintf("[blue]• 90d   Downloads:[-] %s\n", p.Sprintf("%d", info.Analytics90dDownloads))
-
-	return title
+	s.layout.GetDetails().SetContent(info)
 }
 
 func (s *AppService) forceRefreshResults() {
@@ -324,8 +197,7 @@ func (s *AppService) setResults(data *[]models.Formula, scrollToTop bool) {
 			versionCell.SetTextColor(tcell.ColorOrange)
 		}
 
-		downloads := message.NewPrinter(language.English).Sprintf("%d", info.Analytics90dDownloads)
-		downloadsCell := tview.NewTableCell(downloads).SetSelectable(true).SetAlign(tview.AlignRight)
+		downloadsCell := tview.NewTableCell(fmt.Sprintf("%d", info.Analytics90dDownloads)).SetSelectable(true).SetAlign(tview.AlignRight)
 
 		s.layout.GetTable().View().SetCell(i+1, 0, nameCell.SetExpansion(0))
 		s.layout.GetTable().View().SetCell(i+1, 1, versionCell.SetExpansion(0))
